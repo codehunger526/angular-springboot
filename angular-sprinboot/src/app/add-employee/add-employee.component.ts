7dd9ee81e21faab5517408ffc37273459edd5e94
bhoomisingh9 <bhoomi.s@ducoconsultancy.com>
2021-12-29 11:55:24 +0530

    added angular spring boot project

---
+++ angular-sprinboot/src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  saveEmployee(){
+    this.employeeService.addEmployee(this.employee).subscribe( data =>{
+      console.log(data);
+      this.goToEmployeeList();
+    }); 
+  }
+
+  goToEmployeeList(){
+    this.router.navigate(['/employees']);
+  }
+  
+  onSubmit(){
+    console.log(this.employee);
+    this.saveEmployee();
+  }
+}
